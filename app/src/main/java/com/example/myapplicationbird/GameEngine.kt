cb052db5e41a88349dce7b814b0912867921db1b
pathumtharaka2000 <it22600516@my.sliit.lk>
2024-04-28 20:41:08 +0530

    new

--- app/src/main/java/com/example/myapplicationbird/GameEngine.kt
+++ app/src/main/java/com/example/myapplicationbird/GameEngine.kt
@@ -4,6 +4,10 @@ import android.graphics.Canvas
 
 class GameEngine {
     private val backgroundImage = BackgroundImage()
+    private val bird = Bird()
+    companion object {
+        var gameState: Int = 0
+    }
 
     fun updateAndDrawBackgroundImage(canvas: Canvas) {
         backgroundImage.setX(backgroundImage.getX() - backgroundImage.getVelocity())
@@ -15,12 +19,32 @@ class GameEngine {
             backgroundImage.getX().toFloat(),
             backgroundImage.getY().toFloat(),
             null
-            if (backgroundImage.getX()<-(AppConstants.getBitmapBank().getBackgroundWidth()-AppConstants.SCREEN_WIDTH)) {
-
-                canvas.drawBitmap(AppConstants.getBitmapBank().getBackground(), left:backgroundImage.getX() +
-                        AppConstants.getBitmapBank().getBackgroundWidth(),backgroundImage.getY(),paint:null);
-            }
-
         )
+        if (backgroundImage.getX() < -(AppConstants.getBitmapBank().backgroundWidth - AppConstants.SCREEN_WIDTH)) {
+            canvas.drawBitmap(
+                AppConstants.getBitmapBank().background,
+                backgroundImage.getX() + AppConstants.getBitmapBank().backgroundWidth.toFloat(),
+                backgroundImage.getY().toFloat(),
+                null
+            )
+        }
+    }
+
+    fun updateAndDrawBird(canvas: Canvas) {
+        if (gameState == 1) {
+            // No implementation provided
+        }
+        if (bird.getY() < (AppConstants.SCREEN_HEIGHT - AppConstants.getBitmapBank().getBirdHeight()) || bird.getVelocity() < 0) {
+            bird.setVelocity(bird.getVelocity() + AppConstants.gravity)
+            bird.setY(bird.getY() + bird.getVelocity())
+        }
+        val currentFrame = bird.currentFrame
+        canvas.drawBitmap(
+            AppConstants.getBitmapBank().getBird(currentFrame),
+            bird.getX().toFloat(),
+            bird.getY().toFloat(),
+            null
+        )
+        bird.currentFrame = (currentFrame + 1) % bird.maxFrame
     }
 }
